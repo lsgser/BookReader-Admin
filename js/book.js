9cdebfe10db6dd4ab11f2fb7835ed0d672dfc895
lsgser <lsg.seritili@gmail.com>
2020-10-17 00:56:10 +0200

    More functionality added for book recommendations

--- js/book.js
+++ js/book.js
@@ -1,14 +1,16 @@
 function loadBook(){
 	$('#content').append(
-		`<div class="text-center logo" >
+	`	<div class="text-center logo" >
 			<img src="`+R_URL+logo+`" class="rounded" width="10%" height="10%"/>
 		</div>
 		<div class="d-flex justify-content-center">
 			<h4 class="mt-2">Book(s)</h4>
 		</div>
 		<hr>
+		<div class="container">
 		<label for="recipient-name" class="col-form-label">Search for a book</label>
-		<input type="text" placeholder="Search book" class="form-control mt-3 book-name">
+			<input type="text" placeholder="Search book" class="form-control mt-3 book-name">
+		</div>
 		<hr>
 		<div class="container">
 			<div class="row book-list">
@@ -78,11 +80,50 @@ function loadBook(){
 		    </div>
 		  </div>
 		</div>
+
+		<div class="modal fade" id="recommendModal" tabindex="-1" role="dialog" aria-labelledby="recommendModalLabel" aria-hidden="true">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title" id="recommendModalLabel">Add book to module</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+			       <div class="alert alert-danger recommend-error" role="alert"></div>
+		      		<div class="alert alert-success recommend-success" role="alert"></div>
+		      		<div class="container book-info">
+		      		</div>
+		      		<div class="form-group">
+		      			<label for="schoolFormControlSelect">Institution</label>
+		      			<select class="form-control" id="school_select">
+		      			</select>
+		      		</div>
+		      		<div class="form-group">
+		      			<label for="moduleFormControlSelect" class="module-label">Module</label>
+		      			<select class="form-control" id="module_select">
+		      			</select>
+		      		</div>
+		          	<div class="d-flex justify-content-center">
+				    	<div class="spinner-border text-success recommend-spinner">
+				    	</div>
+				    </div>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
 		`
 	)
 
 	hideError()
+	$('.recommend-error').hide()
+	$('.recommend-success').hide()
 	$('.book-search-spinner').hide()
+	$('.recommend-spinner').hide()
 }
 
 function newBookModal(){
@@ -118,16 +159,13 @@ function searchBook(){
 					            -->
 					            <img src="`+S_URL+b.cover_page+`" class="bd-placeholder-img card-img-top" width="80%" height="100%"/>
 					            <div class="card-body">
-					              <h5 class="card-text">Title : `+b.title+`</5>
-					              <h5 class="card-text">Author : `+b.author+`</5>
-					              <h5 class="card-text">Year published : `+b.publish_date+`</5>
-					              <h5 class="card-text">ISBN : `+b.isbn+`</5>
-					              <div class="d-flex justify-content-between align-items-center">
+					              <h6 class="card-text">Title : `+b.title+`</h6>
+					              <h6 class="card-text">Author : `+b.author+`</h6>
+					              <h6 class="card-text">Year published : `+b.publish_date+`</h6>
+					              <h6 class="card-text">ISBN : `+b.isbn+`</h6>
+					              <div class="d-flex justify-content-center align-items-center">
 					                <div class="btn-group">
-					                  <!--
-					                  <button type="button" class="btn btn-sm btn-outline-secondary" onclick="goToSchool()">View</button>
-					                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-					                  -->
+					                  <button type="button" class="btn btn-sm btn-outline-secondary mt-3" onclick="recommendModal('`+b.isbn+`','`+b.cover_page+`','`+b.title+`','`+b.author+`','`+b.publish_date+`')">Add to module</button>					                  
 					                </div>
 					              </div>
 					            </div>
@@ -186,4 +224,105 @@ function addBook(){
 		writeErrorText('Fill in all the required fields')
 		showError()
 	}
+}
+
+function recommendModal(isbn,cover_page,title,author,date){
+	$('.book-info').hide()
+	$('.book-info').html("")
+	$('.book-info').append(`
+		<img src="`+S_URL+cover_page+`" class="bd-placeholder-img" width="50%" height="80%"/>
+		<br>
+		<hr>
+	    <small class="">Title : `+title+`</small>
+	    <br>
+	    <hr>
+	    <small class="">Author : `+author+`</small>
+	    <br>
+	    <hr>
+	    <small class="">Year published : `+date+`</small>
+	    <br>
+	    <hr>
+	    <small class="">ISBN : `+isbn+`</small>
+	    <hr>
+	`)
+	$('#recommendModal').modal('show')
+	$('.book-info').show()
+	$('#school_select').html("")
+	$("#module_select").html("")
+	$('#module_select').hide()
+	$('.module-label').hide()
+	$('.recommend-error').hide()
+	$('.recommend-success').hide()
+	axios.get(URL+'schools').then(res=>{
+		$('#school_select').append(`<option selected>Select institution</option>`)
+		res.data.forEach((s,index)=>{
+			$('#school_select').append(`
+				<option onclick="getSchool('`+s.school+`','`+isbn+`')">`+s.school+`</option>
+			`)
+		})	
+	})
+}
+
+function getModules(school,book){
+	$('.recommend-error').hide()
+	$('.recommend-success').hide()
+	$('.module-label').hide()
+	$("#module_select").html("")
+	$('#module_select').hide()
+	$('.user-details').hide()
+	
+	axios.get(URL+"modules_s/"+school).then(res=>{
+		$('#module_select').append(`
+			<option selected>Select module</option>
+		`)
+		res.data.forEach((m,index)=>{
+			$('#module_select').append(`
+				<option onclick="recommendBook(`+m.id+`,'`+book+`')">`+m.module+`</option>
+			`)
+		})
+		if (res.data.length==0){
+			$('.recommend-error').text("No module for this institution")
+			$('.recommend-error').show()	
+		}else{
+			$('.module-label').show()
+			$('#module_select').show()
+		}
+		
+	}).catch(err=>{
+		$('.recommend-error').text(err.response.data.status)
+		$('.recommend-error').show()
+	})
+}
+
+function getSchool(school,book){
+	$("#module_select").html("")
+	$('#module_select').hide()
+	$('.module-label').hide()
+	$('.recommend-error').hide()
+	$('.recommend-success').hide()
+	axios.get(URL+"school/"+school).then(res=>{
+		getModules(res.data.id,book)
+	}).catch(err=>{
+		$('.recommend-error').text(err.response.data.status)
+		$('.recommend-error').show()
+	})
+}
+
+function recommendBook(m,b){
+	$('.recommend-error').hide()
+	$('.recommend-success').hide()
+	$('.recommend-spinner').show()
+	axios.post(URL+"new_recommend",JSON.stringify({
+		book:b,
+		module:m,
+		token:localStorage.getItem('token')
+	})).then(res=>{
+		$('.recommend-spinner').hide()
+		$('.recommend-success').text("Book added as a recommended book for the selected module")
+		$('.recommend-success').show()
+	}).catch(err =>{
+		$('.recommend-spinner').hide()
+		$('.recommend-error').text(err.response.data.status)
+		$('.recommend-error').show()
+	})
 }
